d98ed22c4941af730421e0486d21e9b2a8319c2d
fenitra <fenitraandrianaivosoa@gmail.com>
2018-04-12 18:02:40 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# nokogiri_1
-Mon premier app pour Nokogiri! :) 
-Je m'appel Andrianaivosoa Malala Harifenitra
+# NOKOGIRI DE FENITRA #
+
+## PRESENTATION ##
+
+** Nom : ** ANDRIANAIVOSOA
+** Prenom : ** Malala Harifenitra
+** Projets : ** 
+	**a - ** Route de la mairie
+		** nom du fichier : ** a_route_de_la_mairie.br
+	**b - ** Trader de l'obscur
+		** nom du fichier : ** b_bitbit.rb
+	**c - ** Route des incubateurs
+		** nom du fichier : ** b_bitbit.rb
+
+# NB : # ##Tapez crtr+c pour arrêter l'execution du programme##
--- b_bitbit.rb
+++ b_bitbit.rb
@@ -8,19 +8,23 @@ def cours_cryptomonnnaies(page_url)
     for i in 0...el_tab.length do
         x = doc.css("#currencies-all tbody tr")[i].text.split("\n").join(" ")[2..-1].split(" ")
         cours.push({
-            :name => x[1],
-            :symbol => x[2],
-            :market_cap => x[3],
-            :price => x[4],
-            :circulating_supply => x[5],
-            :volume => x[6],
-            :percent_1_h => x[7],
-            :percent_24_h => x[8],
-            :percent_7_d =>x[9]
+            :name => x[2],
+            :symbol => x[3],
+            :market_cap => x[4],
+            :price => x[5],
+            :circulating_supply => x[6],
+            :volume => x[7],
+            :percent_1_h => x[8],
+            :percent_24_h => x[9],
+            :percent_7_d =>x[10]
         })
-        puts cours
     end
     cours
 end
 
-puts cours_cryptomonnnaies("https://coinmarketcap.com/all/views/all/")
+puts "Soit patient en attendant l'execution du code! ;)"
+# boucle par heure :) 
+while 0==0
+  puts cours_cryptomonnnaies("https://coinmarketcap.com/all/views/all/")
+  sleep 3600 # 3600second = 1heure
+end
